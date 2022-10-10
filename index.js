b6f0dea1b39797f16a804bbf2b967c2e1360b4a9
RuneKodehode <112623367+RuneKodehode@users.noreply.github.com>
2022-10-10 21:46:02 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ let drop = 0;
 let navbarOne = document.getElementById("dropDownOne");
 let navbarTwo = document.getElementById("dropDownTwo");
 
+
 function nav() {
   if (!drop) {
-    navbarOne.style.opacity = "1";
-    navbarTwo.style.opacity = "1";
-    navbarOne.style.padding = "20px";
-    navbarTwo.style.padding = "20px";
+    navbarOne.style.opacity = "0.95";
+    navbarTwo.style.opacity = "0.95";
+    navbarOne.style.padding = "20px 20px 20px 10px";
+    navbarTwo.style.padding = "20px 20px 20px 10px";
     drop = 1;
-    console.log("wtf");
   } else {
     navbarOne.style.padding = "0px";
     navbarTwo.style.padding = "0px";
