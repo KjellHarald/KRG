aa493e101c8ced16b3752c579c42be32af734beb
RuneKodehode <rune.kodehode@gmail.com>
2022-10-10 14:52:13 +0200

    første versjon av navbar

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-asdasdas;
+let drop = 0;
+let navbarOne = document.getElementById("dropDownOne");
+let navbarTwo = document.getElementById("dropDownTwo");
+
+function nav() {
+  if (!drop) {
+    navbarOne.style.opacity = "1";
+    navbarTwo.style.opacity = "1";
+    navbarOne.style.padding = "20px";
+    navbarTwo.style.padding = "20px";
+    drop = 1;
+    console.log("wtf");
+  } else {
+    navbarOne.style.padding = "0px";
+    navbarTwo.style.padding = "0px";
+    navbarOne.style.opacity = 0;
+    navbarTwo.style.opacity = 0;
+    drop = 0;
+  }
+}
+
+function about() {}
+function contact() {}
